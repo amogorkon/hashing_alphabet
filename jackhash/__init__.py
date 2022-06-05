81a36994b1b5204a2b7227738a2c263e1cb9ead7
Anselm Kiefner <amogorkon@users.noreply.github.com>
2022-06-05 16:33:50 +0200

    Update __init__.py
    Why would we need use here.. stupid me

--- jackhash/__init__.py
+++ jackhash/__init__.py
@@ -2,7 +2,6 @@
 The JACK hash alphabet. 
 For details see https://pypi.org/project/jackhash
 """
-import use
 
 # korean characters: https://github.com/arcsecw/wubi/blob/master/wubi/cw.py
 # chinese characters: https://github.com/tsroten/zhon/blob/develop/zhon/cedict/all.py
